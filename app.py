3c0c319653ef3b089b69a768eb29e63bc183597f
josndan <81441553+josndan@users.noreply.github.com>
2021-12-06 15:08:28 -0500

    Feature - Read pdf files

---
+++ app.py
@@ -0,0 +1,36 @@
+"""
+Parse resume -> P
+Give P as input to a ML Model
+The model can be a classifier for classifying a resume to a project based on the project description
+It can also be an recommender (Like k nearest neighbour) model which selects the top n candidates who are best fit to join build.
+https://pdf.sciencedirectassets.com/280203/1-s2.0-S1877050920X00056/1-s2.0-S187705092030750X/main.pdf?X-Amz-Security-Token=IQoJb3JpZ2luX2VjEIX%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJIMEYCIQCrfdwBJIu7xY6pD9SIazKqmLe36AU1KibmHG0ePBJtYAIhANi7BWD5qtlXOFd8JG%2BtAP6oMh54QBtQjnTQc7jtMknbKvoDCE4QBBoMMDU5MDAzNTQ2ODY1IgyVwm55NzPL8vKzsekq1wNB9rx1JvnDbpXb5ZHmcjCcFSCuzu2QfD6quhjNIjgjXerabkI1P52d%2F0%2FCjSm3YSMm6BRpdwgjim9nr3AGRtjQyS1pOKNkKyE3ld08mwIdBgXXEr1hA9QMcluz41kmZumI03mM0q6Xa0H3%2FBRQgGOVjPphLI72jrMIdt%2FPtJE6uHesW4Ao7uBXH%2FezHPDn5ht%2Fo2V0gFjwMWx%2BfnZ%2FMrenzgnxU0ZsuIBIV75Mp09x3WBCaoUFQEwdHr0WIpVjN37ZTycA6wxmwzZF%2FgvT9oUIuNIK43AgqehGaq1ywcPEqqK%2B6Pz%2BBAg48JSp%2FfhB5FtAxYwL2rmdWRIFBSCxOYRloNTj5fCAVXTTLXzgMZ1WnhmZEApk%2BCC2MUu71OcDLSS%2FTZ%2B4ga4i6OMVHePn8ahFXDSJG2aBPzL1vMnmLulwbtUDuhmkJp7NsGGT8eKiJLRIGXU8eVcbxmqwa7l8Ve%2BNkK9kMxvNkPksROnRkl5xbJcbZRketW55mSebZi5KCjULD2eTi2F85xjrn%2BOd3RDHMPYywqyU%2Fh%2Bi32K32wyd0fO8iXXU%2Bwe5inxn7yAhulhP05esf6dXzgt%2FWp7wREaPW17s6CcXDf098lljfFHEHXlSuB%2Fb6ikwoKX1jAY6pAE7DWmXXZNdzj1p4k1SweaeCR1JZ654mVZQC0d7lQ3qaadTMAyunaT6JYeuAxFu0flPVgmx31gqHa%2FMe6JTbyrZ3%2BdgSD7pGAH6AdG90t5aujDjo27G3nUaaPf24mVH0UmusBXMi4J3XQLBcxIRWda3VJjCHdDHUUGG1jCL5gq7g60PBBmEDoduZjuEgde9Ahi953fmu%2BOP%2BfzT682QHu%2Bnd%2FIgsA%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20211123T220834Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAQ3PHCVTYZA53DL72%2F20211123%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=0964d61a10404559e3a0ddb6c2bed92755bd49c595445ead04e3043977e51228&hash=c90ac608380b18548a8dd40b59e285d47b1d7e37640fdb7633fe8bff550866e8&host=68042c943591013ac2b2430a89b270f6af2c76d8dfd086a07176afe7c76c2c61&pii=S187705092030750X&tid=spdf-21b5a3db-caa0-46b4-8c17-5566741c88e1&sid=41d3578c8641f649ac396f39891b90c0dd01gxrqa&type=client
+"""
+
+'''
+Skills
+GPA
+companies names
+'''
+
+import pdfplumber
+import spacy
+from spacy.matcher import Matcher
+
+file_name = input("Enter the location of the resume (PDF)")
+
+with pdfplumber.open(file_name) as pdf:
+    first_page = pdf.pages[0]
+    text = first_page.extract_text()
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    matcher = Matcher(nlp.vocab)
+    pattern = [
+        {},
+        {},
+        {}
+    ]
+    matcher.add("")
+    for ent in doc.ents:
+        print(ent.text,ent.label_)
+
+
